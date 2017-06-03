32a60fb691e667a6c6658a236e366e6419dda2c5
ziaochina <ziaochina@gmail.com>
2017-06-03 16:14:46 +0800

    zlj

--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,75 @@ var webpack = require("webpack");
 var path = require("path");
 
 var plugins = [
-  new webpack.DefinePlugin({
-    "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
-  })
+    new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+    })
 ];
 
 
 if (process.env.COMPRESS) {
-  plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      }
-    })
-  );
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false
+            }
+        })
+    );
 }
 
 
 module.exports = {
-  entry: ["./src/index.js"],
+        entry: ["./src/index.js"],
 
-  output: {
-    path: path.join(__dirname, "/dist/"),
-    library: "XRComponent",
-    libraryTarget: "umd"
-  },
+        output: {
+            path: path.join(__dirname, "/dist/"),
+            library: "XRComponent",
+            libraryTarget: "umd"
+        },
 
-  resolve: {
-    extensions: [".js"]
-  },
+        resolve: {
+            extensions: [".js"]
+        },
 
-  externals: {
-    "react": "React",
-    "react-dom": "ReactDom",
-  },
+        externals: {
+            "react": "React",
+            "react-dom": "ReactDom",
+        },
 
-  module: {
-    loaders: [
-      { test: /\.js?$/, exclude: /node_modules/, loader: "babel-loader" }
-    ]
-  },
+        module: {
+            rules: [{
+                test: /\.css$/,
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'css-loader'
+                }]
+            }, {
+                test: /\.less$/,
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'css-loader'
+                }, {
+                    loader: 'less-loader'
+                }]
+            }, {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }, {
+                test: /\.md$/,
+                use: [{
+                    loader: "html-loader"
+                }, {
+                    loader: "highlight-loader"
+                }, {
+                    loader: "markdown-loader",
 
-  plugins: plugins
+                }]
+            }],
+        },
+    },
+
+    plugins: plugins
 };
