612d765ff9144dab62a340ef9e153f9563bf3f93
ziaochina <ziaochina@gmail.com>
2017-06-06 13:24:58 +0800

    zlj

--- examples/src/apps/icon/example/example1.js
+++ examples/src/apps/icon/example/example1.js
@@ -6,7 +6,7 @@ export default class Example1 extends Component {
 		let style = {fontSize: 16, color: '#08c', marginRight:20}
 		return (
 			<div>
-				<Icon type='github' style={style} />
+				<Icon type='github' disabled style={style} />
 				<Icon type='down' style={style}/>
 				<Icon type='up' style={style}/>
 				<Icon type='left' showStyle='primary' style={style}/>
--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -3,11 +3,21 @@ import {Icon} from 'antd'
 import classNames from 'classnames'
 
 export default function IconComponent(props){
+	if(props.visible === false)
+		return null
+
+	let showStyle = props.showStyle || 'default'
+
+	if(props.disabled){
+		showStyle = 'disabled'
+	}
+
 	let className = classNames({
 		[props.fontFamily] : !!props.fontFamily,
 		[`${props.fontFamily}-${props.type}`] : !!props.fontFamily,
-		[`xricon--${props.showStyle}`] : !!props.showStyle,
+		[`xricon--${pshowStyle}`] : !!showStyle,
 		[props.className] : !!props.className
 	})
+	
 	return <Icon {...props} className={className} />
 }
