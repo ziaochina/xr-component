ff61e39e99f6936ba99cfd0e7ddaeffd68561b1f
ziaochina <ziaochina@gmail.com>
2017-06-11 07:50:07 +0800

    'zlj'

---
+++ examples/src/apps/input/example/example2.js
@@ -0,0 +1,40 @@
+import React,{Component} from 'react'
+import ReactDOM from 'react-dom' 
+import { Input, Icon  } from 'xr-component'
+
+export default class Example1 extends Component {
+	constructor(props) {
+   	 	super(props)
+    	this.state = {
+      		userName: ''
+    	}
+  	}
+
+	emitEmpty(){
+		let dom = ReactDOM.findDOMNode(this.userNameInput)
+	    dom.querySelector('input').focus()
+	    this.setState({ userName: '' })
+	}
+
+  	handleChangeUserName(e){
+    	this.setState({ userName: e.target.value })
+  	}
+
+	render() {
+		const { userName } = this.state
+    	const suffix = userName ? <Icon type="close-circle" onClick={::this.emitEmpty} /> : null
+
+		return (
+			<div>
+		 		<Input
+			        placeholder="Enter your userName"
+			        prefix={<Icon type="user" />}
+			        suffix={suffix}
+			        value={userName}
+			        onChange={::this.handleChangeUserName}
+			        ref={node => this.userNameInput = node}
+		      	/>
+		  	</div>
+		)
+	}
+}
