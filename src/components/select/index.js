938e6edc92331c2db7902a3f64f7881b2aa17c0a
ziaochina <ziaochina@gmail.com>
2017-06-12 13:08:32 +0800

    'zlj'

--- src/components/select/index.js
+++ src/components/select/index.js
@@ -3,11 +3,42 @@ import Select from 'ziaochina-rc-select'
 import classNames from 'classnames'
 
 function SelectComponent(props){
-	let className = classNames({
+	let {className, notFoundContent, optionLabelProp, 
+		mode, combobox, tags, multiple, size, ...otherProps} = props
+
+	className = classNames({
+		'ant-select': true,
 		'xr-select' :true,
-		[props.className] : !!props.className
+		[`ant-select-lg`]: size === 'large',
+      	[`ant-select-sm`]: size === 'small',
+		[className] : !!className
 	})
-	return <Select {...props} className={className} />
+
+	const isCombobox = mode === 'combobox' || combobox
+
+	notFoundContent = notFoundContent || '无匹配结果'
+
+	if (isCombobox) {
+      notFoundContent = null;
+      // children 带 dom 结构时，无法填入输入框
+      optionLabelProp = optionLabelProp || 'value'
+    }
+
+    const modeConfig = {
+      	multiple: mode === 'multiple' || multiple,
+      	tags: mode === 'tags' || tags,
+      	combobox: isCombobox,
+    }
+
+
+	return (<Select 
+		{...otherProps} 
+		{...modeConfig} 
+		prefixCls='ant-select' 
+		className={className} 
+		optionLabelProp={optionLabelProp || 'children'}
+		notFoundContent={notFoundContent}
+	/>)
 }
 
 SelectComponent.Option = Select.Option
